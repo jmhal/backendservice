73edb2cefdba0515714dab60daef8d4be23a4470
jmhal <jmhal@jupiter.cosmos>
2017-01-07 07:57:41 -0300

    New hierarchy for cluster.

--- base/platform/infrastructure.py
+++ base/platform/infrastructure.py
@@ -1,31 +1,56 @@
+"""
+As basic infrastructure, we consider a cluster, that might be virtual or physical.
+These classes are abstract. In the final scenario, a maintainer should derive these
+classes to represent it's real infrastructure.
+"""
 
 """
-This is an Abstract Class for all infrastructures.
+This is a cluster infrastructure. It represents the whole infrastructure available
+to the computational system. 
+- If it is a virtual Cluster, nodes may be added. Also, elasticity may change the configuration of these nodes and the assignment of nodes to components. 
+- If it is a physical cluster, we cannot alter the nodes' configuration.
+
 """
-class Infrastructure(object):
+class Cluster: 
    def __init__(self):
+      """
+      The extended class must initialize the set of nodes for the cluster.
+      There are several options. The maintainer may hard code the configuration or
+      keep it in a configuration file.
+      """
       raise NotImplementedError("Abstract Class!")
-
-class ComputingResource(object):
-   def __init__(self):
-      raise NotImplementedError("Abstract Class!")
-   
-
-"""
-This is a cluster infrastructure. 
-"""
-class Cluster(Infrastructure): 
-   def __init__(self):
 
    def isVirtual(self):
+      """
+      Should return True if this cluster
+      """
+      raise NotImplementedError("Abstract Class!")
 
-   def addNodes(self, N):
+   def getNodes(self):
+      """
+      Return the nodes, as a dictionnary, {NodeID: NodeObject}
+      """
+      raise NotImplementedError("Abstract Class!")
 
-   def removeNodes(self, N):
+class VirtualCluster(Cluster):
+   def __init__(self):
+      raise NotImplementedError("Abstract Class!")
 
+   def addNode(self, flavor):
+      raise NotImplementedError("Abstract Class!")
 
-   def removeNode(self):
+   def removeNode(self, nodeId):
+      raise NotImplementedError("Abstract Class!")
 
-   def updateNodeConfiguration(self, memory, cores, storage):
+class PhysicalCluster(Cluster):
+   def __init__(self):
+      raise NotImplementedError("Abstract Class!")
 
+"""
+Every cluster is made of nodes. We assume a homogeneous in certain aspects, for example, 
+processor architecture.
+"""
+class Node:
+   def __init__(self):
+      raise NotImplementedError("Abstract Class!")
 
