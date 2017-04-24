6f1783d468f0530b956275ba8d4bc2bb3b23e731
jmhal <jmhal@jupiter.cosmos>
2017-04-24 17:08:58 -0300

    Now, creating a component for Matrix Multiplication.

--- base/computation/malleable/__init__.py
+++ base/computation/malleable/__init__.py
@@ -1,32 +1,34 @@
 import CCAPython.gov.cca
-import logging
+from elastichpc import logging
 from multiprocessing import Process, Value, Array, Pipe
 
 # Setting Up Log
 logger = logging.getLogger('root')
 
-def manager_unit(manager_conn, computation):
+def manager_unit(component):
    """
    This method is the manager unit. It will dispatch requests from
    the environment to the other parallel units running the computation
    It is the same for every component. In the end, it defines an interface
    to control the computations.
    """
+   manager_conn = component.manager_conn
+   computation = component.computation_impl
    while True:
       message = manager_conn.recv()
       logger.debug("Manager Unit Got Message: " + str(message))
       if message["action"] == "start":
          resources = message["resources"]
-         state = message["state"]
-         start_status = computation.start(resources, state)  
+         input_data = message["input_data"]
+         start_status = computation.start(resources, input_data)  
          manager_conn.send({"start_status": start_status})
       elif message["action"] == "progress":
          progress = computation.getProgress()
          manager_conn.send({"progress_status" : progress})
       elif message["action"] == "update_resources":
          new_resources = message["new_resources"]
-         new_resources_status = computation.setNewResources(new_resources)
-         manager_conn.send({"new_resources_status" : new_resources_status})
+         new_resources_status = computation.updateResources(new_resources)
+         manager_conn.send({"update_resources_status" : new_resources_status})
       elif message["action"] == "persist":
          persist_status = computation.persist()
          manager_conn.send({"persist_status" : persist_status})
@@ -35,48 +37,70 @@ def manager_unit(manager_conn, computation):
          manager_conn.send({"result_status" : result_status})
       elif message["action"] == "stop":
          stop_status = computation.stop()
-         manager_conn.send({"stop_status" : stop_status}]
+         manager_conn.send({"stop_status" : stop_status})
          break
    manager_conn.close()
    return  
 
-class ComputationUnit():
+class ComputationUnit(object):
+   """
+   This is an interface that defines methods for controlling the unit.
+   """
+   def __init__(self):
+      raise NotImplementedError("Interface.")
+     
+   def start(self, resources, input_data):
+      raise NotImplementedError("Interface.")
+
+   def getProgress(self, new_resources):
+      raise NotImplementedError("Interface.")
+
+   def updateResources(self, new_resources):
+      raise NotImplementedError("Interface.")
+    
+   def persist(self):
+      raise NotImplementedError("Interface.")
+
+   def getResults(self):
+      raise NotImplementedError("Interface.")
+
+   def stop(self):
+      raise NotImplementedError("Interface.")
+
+class ComputationUnitImpl(ComputationUnit):
    """
    This is an abstract class that must be overwritten for every component.
    I'm writing this with MPI computations in mind, where the MPI processes
-   will be launched with the help of the subprocess package. Although right 
-   now I'm not dealing with the environment ports, this class should also 
-   be responsible for setting up a web services container. 
+   will be launched with the help of the subprocess package. 
    """
-   def __init__(self):
-      raise NotImplementedError("Abstract Class.")
-     
-   def start(self, resources, state):
-      raise NotImplementedError("Abstract Class.")
+   pass
 
-   def setNewResources(self, new_resources):
-      raise NotImplementedError("Abstract Class.")
-
-   def persist(self):
-      raise NotImplementedError("Abstract Class.")
-
-   def stop(self):
-      raise NotImplementedError("Abstract Class.")
-
-   def getResults(self):
-      raise NotImplementedError("Abstract Class.")
-
-
-class ReconfigurationPort(CCAPython.gov.cca.Port):
-   """
-   The malleable computation component provides this port for reconfiguration from the platform.
-   """
-   def __init__(self, portType,  component):
+class ComputationUnitProxy(ComputationUnit):
+   def __init__(self, component)
       self.component = component
-      super(ReconfigurationPort, self). __init__(portType)
       return
+  
+  def start(self, input_data = None):
+      """
+      Starts or restarts the computation. The state holds the path to a file inside the root node.
+      It it up to the developer to define how to start the components units. 
+      """
+      allocation_port = self.component.services.getPort("AllocationPort")
+      resources = allocation_port.getResources()
+      
+      logger.debug("Starting Computation: Resources %s, State %s" % (str(resources), str(input_data)))
+      manager_process = Process(target = manager_unit, args=(self.component))
+      manager_process.daemon = True
+      manager_process.start();
 
-   def getComputationProgress(self):
+      self.component.manager_process = manager_process
+
+      self.component.framework_conn.send({"action": "start", "resources" : resources, "input_data": input_data})
+      message = self.component.framework_conn.recv()
+
+      return message["start_status"]
+  
+   def getProgress(self, new_resources):
       """
       Will return to the Platform (and the internal Reconfiguration) the progress of the Computation.
       """
@@ -92,14 +116,8 @@ class ReconfigurationPort(CCAPython.gov.cca.Port):
       logger.debug("Updating Resources: " + str(resources))
       self.component.framework_conn.send({"action" : "update_resources", "new_resources" : resources})
       message = self.component.driver_conn.recv()
-      return message["new_resource_status"]
-
-class ExecutionControlPort(CCAPython.gov.cca.Port):
-   def __init__(self, portType, component):
-      self.component = component
-      super(ExecutionControlPort, self).__init__(portType)
-      return
-      
+      return message["update_resources_status"]
+   
    def persist(self):
       """
       It is up to the developer to define a persistence mechanism. 
@@ -110,23 +128,13 @@ class ExecutionControlPort(CCAPython.gov.cca.Port):
       message = self.component.framework_conn.recv()
       return message["persist_status"]
 
-   def start(self, state = None):
+   def getResults(self):
       """
-      Starts or restarts the computation. The state holds the path to a file inside the root node.
-      It it up to the developer to define how to start the components units. 
+      Returns the result of the computation.
       """
-      allocation_port = self.component.services.getPort("AllocationPort")
-      resources = allocation_port.getResources()
-      
-      logger.debug("Starting Computation: Resources %s, State %s" % (str(resources), str(state)))
-      computation_process = Process(target = manager_unit, args=(self.component.compute_conn, self.component.computation))
-      computation_process.daemon = True
-      computation_process.start();
-
-      self.component.framework_conn.send({"action": start, "resources" : resources, "state": state})
+      self.component.framework_conn.send({"action" : "getResults"})
       message = self.component.framework_conn.recv()
-
-      return message["start_status"]
+      return message["result_status"]
 
    def stop(self):
       """
@@ -136,23 +144,23 @@ class ExecutionControlPort(CCAPython.gov.cca.Port):
       message = self.component.framework_conn.recv()
       return message["stop_status"]   
 
-   def getResults(self):
-      """
-      Returns the result of the computation.
-      """
-      if self.isFinished():
-         self.component.framework_conn.send({"action" : "getResults"})
-         message = self.component.framework_conn.recv()
-         return message["result_status"]
-      else return None
 
-   def isFinished(self):
-      """
-      The return of getComputationProgress must be a number.
-      """
-      if (self.component.reconfiguration_port.getComputationProgress() >= 1.0):
-         return True
-      return False
+class ReconfigurationPort(CCAPython.gov.cca.Port):
+   """
+   The malleable computation component provides this port for reconfiguration from the platform.
+   It provides a method for monitoring and updating the resources. A Computation Component might
+   personalize it further, but I believe the implementations provided are enough for most cases.
+   """
+   def __init__(self, portType,  component):
+      self.component = component
+      super(ReconfigurationPort, self). __init__(portType)
+      return
+
+   def getComputationProgress(self):
+      return component.computation_proxy.getProgress() 
+
+   def updateResources(self, resources):
+      return component.computation_proxy.updateResources(resources)
 
 class MalleableComputationComponent(CCAPython.gov.cca.Component):
    """
@@ -161,22 +169,25 @@ class MalleableComputationComponent(CCAPython.gov.cca.Component):
    def __init__(self):
       """
       Creates the reconfiguration and execution ports, and the communication pipe.
+      A computation component deriving this class must perform two extra tasks:
+      - Assign self.computation_impl to a rightful implementation of ComputationUnitImpl.
+      - Create any other ports.
       """
-      framework_conn, computation_conn = Pipe()
+      framework_conn, manager_conn = Pipe()
       self.framework_conn = framework_conn
-      self.computation_conn = compute_conn
-
-      # This line must be rewritten for the actual Computation Component
-      self.computation = ComputationUnit()
+      self.manager_conn = manager_conn
+      self.computation_proxy = ComputationUnitProxy(self)
+      self.computation_impl = None 
 
       self.reconfiguration_port = ReconfigurationPort("elastichpc.base.computation.malleable.ReconfigurationPort", self)
-      self.execution_control_port = ExecutionControlPort("elastichpc.base.computation.malleable.ExecutionControlPort", self)
       return
 
    def setServices(self, services):
+      """
+      If any other port is created by a deriving component, they should also be registered. 
+      """
       self.services = services
       services.addProvidesPort(self.reconfiguration_port, "ComputationReconfigurationPort", "elastichpc.base.computation.malleable.ReconfigurationPort", None)
-      services.addProvidesPort(self.execution_control_port, "ExecutionControlPort", "elastichpc.base.computation.malleable.ExecutionControlPort", None)
       services.registerUsesPort("AllocationPort", "elastichpc.base.platform.malleable.AllocationPort", None)
       return 
 
---
+++ examples/MalleableMatrixMultiplication/__init__.py
@@ -0,0 +1,59 @@
+import os
+import CCAPython.gov.cca
+import elastichpc.base.computation.malleable.MalleableComputationComponent
+import elastichpc.base.computation.malleable.ComputationUnitImpl
+
+from elastichpc import logging
+
+# Setting Up Log
+logger = logging.getLogger('root')
+
+class MatrixMultiplicationWorkQueue(base.computation.malleable.ComputationUnitImpl):
+   pass
+
+class MatrixSetup(CCAPython.gov.cca.Port):
+   """
+   Environment Port
+   """
+   def __init__(self, portType, component):
+      self.component = component
+      super(MatrixSetup, self).__init__(portType)
+
+   def setMatrixFileNames(self, matrix_A_filename, matrix_B_filename, matrix_C_filename): 
+      logger.debug("Matrix A %s, Matrix B %s, Matrix %s" % (matrix_A_filename, matrix_B_filename, matrix_C_filename))
+      if os.path.isfile(matrix_A_filename) and os.path.isfile(matrix_B_filename) and os.path.isfile(matrix_C_filename): 
+         self.component.matrix_A_filename = matrix_A_filename
+         self.component.matrix_B_filename = matrix_B_filename
+         self.component.matrix_C_filename = matrix_C_filename
+         self.component.matrix_set = True
+         return True
+      else:
+         return False
+
+class MatrixMultiplication(CCAPython.gov.cca.Port):
+   """
+   Task Port
+   """
+   def __init__(self, portType, component):
+      self.component = component
+      super(MatrixMultiplication, self).__init__(portType)
+
+   def matrixMultiplication():
+      pass
+
+class MatrixMultiplicationComponent(base.computation.malleable.MalleableComputationComponent):
+   def __init__(self):
+      super(MatrixMultiplicationComponent, self).__init__()
+      self.computation_impl = MatrixMultiplicationWorkQueue()
+      self.matrix_set = False
+      self.matrix_setup_environment_port = MatrixSetup("elastichpc.examples.MalleableMatrixMultiplication.MatrixSetup", self)
+      self.matrix_multiplication_task_port = MatrixSetup("elastichpc.examples.MalleableMatrixMultiplication.MatrixMultiplication", self)
+      return
+
+   def setServices(self, services):
+      super(MatrixMultiplicationComponent, self).setServices(services)
+      services.addProvidesPort(self.matrix_setup_environment_port, "MatrixSetup", "elastichpc.examples.MalleableMatrixMultiplication.MatrixSetup", None)
+      services.addProvidesPort(self.matrix_multiplication_task_port, "MatrixMultiplication", "elastichpc.examples.MalleableMatrixMultiplication.MatrixMultiplication", None)
+      return
+
+
