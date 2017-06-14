bec4e33e103d95f60222e68a68b494bbca4548a7
jmhal <jmarcelo.alencar@gmail.com>
2017-06-14 14:47:13 -0300

    old reference

--- beta/trials/infrastructure/resources/client.py
+++ beta/trials/infrastructure/resources/client.py
@@ -22,7 +22,7 @@ class ResourcesProxy():
       ips = self.proxy.get_ips(self.stack_name, self.stack_id)
       
       self.number_of_nodes = 1
-      machinefile.write(ips['head_node_ip'] + ":2"))
+      machinefile.write(ips['head_node_ip'] + ":2")
       for machine in ips['compute_node_ips']:
          machinefile.write(machine + ":2"))
 	 self.number_of_nodes += 1
