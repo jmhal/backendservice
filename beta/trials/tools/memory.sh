6d36d982d39142625b67ccd1b6171dc8f062b504
jmhal <jmarcelo.alencar@gmail.com>
2017-06-17 13:18:26 +0000

    expanded memory.sh

--- beta/trials/tools/memory.sh
+++ beta/trials/tools/memory.sh
@@ -12,4 +12,16 @@ list=`machine_file`
 memory=$(pdsh -R ssh -w $list free -m 2> /dev/null | grep Mem | awk 'BEGIN{ mem=0 }{mem = mem + $4}END{print mem/NR}')
 cpu=$(pdsh -R ssh -w $list cat /proc/loadavg | awk 'BEGIN { cpu=0.0 } {cpu = cpu + $2} END { if (cpu == 0) {print cpu;} else {print cpu/NR;}}')
 
-echo $cpu:$memory 
+if [ $# -gt 0 ]
+then
+
+   if [ $1 == "cpu" ] 
+   then
+      echo $cpu;
+   elif [ $1 == "memory" ] 
+   then
+      echo $memory;
+   fi
+else
+   echo $cpu:$memory 
+fi
