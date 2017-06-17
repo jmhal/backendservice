f4b4033b2d4abfb5fc2b7fff71c6a1cc7689c0c8
jmhal <jmarcelo.alencar@gmail.com>
2017-06-17 14:00:14 -0300

    testing if the mpi really runs.

--- beta/trials/Computation.py
+++ beta/trials/Computation.py
@@ -36,9 +36,9 @@ def computation_unit(reconfiguration_port, computation_input):
 		 str(m), "10", "0", "teste.mtr_" + str(i)]
       log(str(command))
       # process = subprocess.Popen(command, stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
-      (output, error) = process.communicate()
+      # (output, error) = process.communicate()
       # os.remove(home + "/repositorios/elastichpc/beta/trials/" + "teste.mtr_" + str(m))
-      log("Execution = " + str(output) + "|" + str(error))
+      # log("Execution = " + str(output) + "|" + str(error))
 
       reconfiguration_port.get_sensor().value = float(i + 1) / inputs_size
       log("Progress = " + str(float(i + 1) /inputs_size))
