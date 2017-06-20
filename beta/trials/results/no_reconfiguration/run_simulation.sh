f08fbd2ea297bacdaae25745609b25a6c7239ce5
jmhal <jmarcelo.alencar@gmail.com>
2017-06-20 06:21:45 -0300

    results without reconfiguration.

--- beta/trials/results/no_reconfiguration/run_simulation.sh
+++ beta/trials/results/no_reconfiguration/run_simulation.sh
@@ -2,8 +2,8 @@
 
 for cluster_size in 2 6 8
 do
-   for N in 1000 2000 3000 4000 5000 6000 7000 8000 9000
+   for N in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000
    do
-      echo "./run_system.sh $cluster_size 20 $N"
+      ./run_system.sh $cluster_size 20 $N
    done
 done
